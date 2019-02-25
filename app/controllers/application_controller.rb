ba6be86993c63f3eb53944577e2a2edf6c5ae74d
Thomas Pope <tpope0928@gmail.com>
2019-02-25 19:41:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,9 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
 
 end
