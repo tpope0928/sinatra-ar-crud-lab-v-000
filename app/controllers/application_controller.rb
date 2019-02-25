504bae8ad4d0d410cbe9f7362e4e1b081b9a9f91
Thomas Pope <tpope0928@gmail.com>
2019-02-25 19:42:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{ @article.id }"
   end
 
-  
+
   delete "/articles/:id" do
     Article.destroy(params[:id])
     redirect to "/articles"
